ae3777c373af722f8672bf3416078b9d93599a89
Mark <markejay.am@gmail.com>
2022-11-25 22:37:14 -0500

    done

--- routes/expense.js
+++ routes/expense.js
@@ -16,6 +16,29 @@ router.get("/:id", (req, res, next) => {
     Expense.findById(req.params.id).then(expense => {
         if (expense) {
             res.status(200).json(expense);
+            console.log("hellow")
+        }
+        else {
+            res.status(404).json({ message: "Expense not found" })
+        }
+    })
+})
+// app.put('/quotes', (req, res) => {
+//     quotesCollection.findOneAndUpdate(/* ... */)
+//       .then(result => {
+//          res.json('Success')
+//        })
+//       .catch(error => console.error(error))
+//   }
+
+//delete api
+
+
+
+router.delete("/:id", (req, res, next) => {
+    Expense.findByIdAndDelete(req.params.id).then(expense => {
+        if (expense) {
+            res.status(200).json(expense);
         }
         else {
             res.status(404).json({ message: "Expense not found" })
@@ -98,6 +121,31 @@ router.get("/category/travel", (req, res) => {
     })
 })
 
+router.get("/category/clothing", (req, res) => {
+
+    Expense.find({ category: 'clothing' }).then(expense => {
+        if (expense) {
+            res.status(200).json(expense);
+        }
+        else {
+            res.status(404).json({ message: "Expense not found" })
+        }
+    })
+})
+
+router.get("/category/others", (req, res) => {
+
+    Expense.find({ category: 'others' }).then(expense => {
+        if (expense) {
+            res.status(200).json(expense);
+        }
+        else {
+            res.status(404).json({ message: "Expense not found" })
+        }
+    })
+})
+
+
 router.post('/add-expense', async (req, res) => {
     const { title, price, description, category, purchaseDate } = req.body;
 
@@ -111,4 +159,42 @@ router.post('/add-expense', async (req, res) => {
         console.log(err);
     }
 })
+
+router.put("/:id", (req, res, next) => {
+    const { title, price, description, category, purchaseDate } = req.body;
+    // Expense.findByIdAndUpdate(req.params.id,
+    //     { title, price, description }).then(expense => {
+    //         res.status(200).json(expense);
+    //     })
+    Expense.findById(req.params.id).then(expense => {
+        if (expense) {
+            expense.title = req.body.title;
+            expense.price = req.body.price;
+            expense.description = req.body.description;
+            expense.category = req.body.category;
+            expense.purchaseDate = req.body.purchaseDate;
+            expense.save();
+            console.log(req.body.title)
+            // expense.title = req.body.title;
+            res.status(200).json(expense);
+        }
+        else {
+            res.status(404).json({ message: "Expense not found" })
+        }
+    })
+})
+
+//get data by month
+// router.get("/getDate/", (req, res) => {
+
+
+//     Expense.find({ purchaseDate: ISODate("2022-08-03") }).then(expense => {
+//         if (expense) {
+//             res.status(200).json(expense);
+//         }
+//         else {
+//             res.status(404).json({ message: "Expense not found" })
+//         }
+//     })
+// })
 module.exports = router;
