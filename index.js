a0623d9da9d7944686f25e78a1b10ec284a3acea
Mark Ejakpomewhe <markejay.am@gmail.com>
2023-01-12 02:43:31 -0500

    new port

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ const expenseRoutes = require('./routes/expense')
 app.use(cors())
 app.use(express.json());
 const db = process.env.dbUrl
+
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 5005;
 mongoose.connect(db, { useNewUrlParser: true})
     .then(() => console.log('connected to db'))
     .catch(err => console.log(err))
 
-app.listen(5005, () => console.log('listening on port 5005'))
+app.listen(port, () => console.log('listening on port 5005'))
 
 app.get('/main', (req, res) => {
     res.send('Hello World')
